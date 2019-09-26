5316914a4674b36c169d828fd9771ca1fd0a346c
nag-training <nag-training@172.20.10.2>
2019-09-26 08:38:05 +0530

    app

--- Txr-system/run.sh
+++ Txr-system/run.sh
@@ -1,2 +1,2 @@
 
-java -Xms256m -Xmx256m -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heap/txr-system.hprof -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:txr-system.gc -jar target/txr-system-0.0.1-SNAPSHOT.jar
+java -Xms256m -Xmx256m -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heap/txr-system.hprof -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:txr-system.gc -jar txr-system-0.0.1-SNAPSHOT.jar
